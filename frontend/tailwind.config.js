a985737ee4049e829797692e8dd4da8c9138fcf6
Dxeg0o <dsoler.olguin@gmail.com>
2024-10-11 16:57:56 -0300

    changes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Incluye todos los archivos fuente de React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
